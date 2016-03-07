4441ad290b850c593dff02cb9f24ff5cf75b3cb8
HungPhamLZD <hung.pham@lazada.com>
2016-03-07 13:24:17 +0700

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,4 +1,4 @@
 <?php
-echo("My name is Hung Pham and this is how it looks like on other languages:")
+echo("My name is Hung Pham and this is how it looks like on other languages:");
 header('Location: https://translate.google.com/#auto/en/H%C6%B0ng%20Ph%E1%BA%A1m');    
 ?>
