020ca7851e0ed8c5b09a086830ff0c2a645ff541
HungPhamLZD <hung.pham@lazada.com>
2016-03-07 12:09:00 +0700

    Added files via upload

---
+++ 1.php
@@ -0,0 +1,3 @@
+<?php
+header('Location: https://translate.google.com/#auto/en/H%C6%B0ng%20Ph%E1%BA%A1m');    
+?>
---
+++ 2.php
@@ -0,0 +1,10 @@
+<?php
+$total = 0;
+$i = 1;
+while($i <= 1000 )
+{
+$total = $total + $i;
+$i++;
+}
+print($total);
+?>
